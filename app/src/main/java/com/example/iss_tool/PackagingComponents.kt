2c67f07fe4e1e9abe0275bb5db1e7aa2418add4b
Zeina Hassoun <zeinahassoun1999@gmail.com>
2024-02-06 00:28:24 +0100

    packaging steps(missing ice only)

--- app/src/main/java/com/example/iss_tool/PackagingComponents.kt
+++ app/src/main/java/com/example/iss_tool/PackagingComponents.kt
@@ -138,7 +138,7 @@ import com.example.iss_tool.theme.white
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
+fun Triplepackagingsystem(modifier:Modifier,category:Category,ice:Boolean){
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -167,13 +167,13 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
                 Text("1. PRIMARY RECEPTABLE",style= customTypography.bodyMedium)
                 Text("- It shall be leakproof;" ,
                     Modifier.align(Alignment.Start),style= customTypography.bodySmall)
-                if(category == "Category A"){
+                if(category == Category.A){
                     Text("- It shall be of glass,metal or plastics;\n" +
                             "- Positive means of ensuring a leakproof seal shall be provided, i.e. a heat seal. a skirted stopper or a metal crimp steal. If screw caps are used, they should be secured by positive means, i.e. tape,paraffin sealing tape or manufactured locking closure.\n" +
                             "Whatever the intended temperature of the consignment, the primary receptacle OR the secondary packaging must be capable of withstanding a pressure differential of not less than 95kPA (0.95 bar), as well astemperatures in the range of -40°C to +55°C." ,
                         Modifier.align(Alignment.Start),style= customTypography.bodySmall)
                 }
-                if(category == "Category B"){
+                if(category == Category.B){
                     Text("- It must be siftproof\n" +
                             "Whatever the intended temperature of the consignment, the primary receptacle OR the secondary packaging must be capable of withstanding a pressure differential of not less than 95kPA (0.95 bar)." ,
                         Modifier.align(Alignment.Start),style= customTypography.bodySmall)
@@ -202,11 +202,11 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
                 Text("2. SECONDARY PACKAGING",style= customTypography.bodyMedium)
                 Text("- It shall be leakproof;",
                     Modifier.align(Alignment.Start),style= customTypography.bodySmall)
-                if(category == "Category A"){
+                if(category == Category.A){
                     Text("Whatever the intended temperature of the consignment, the primary receptacle OR the secondary packaging must be capable of withstanding a pressure differential of not less than 95kPA (0.95 bar), as well astemperatures in the range of -40°C to +55°C." ,
                         Modifier.align(Alignment.Start),style= customTypography.bodySmall)
                 }
-                if(category == "Category B"){
+                if(category == Category.B){
                     Text("- It must be siftproof\n" +
                             "Whatever the intended temperature of the consignment, the primary receptacle OR the secondary packaging must be capable of withstanding a pressure differential of not less than 95kPA (0.95 bar)." ,
                         Modifier.align(Alignment.Start),style= customTypography.bodySmall)
@@ -217,7 +217,7 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
                 alignment = Alignment.Center
             )}
         }
-        if(category == "Category B"){
+        if(category == Category.B){
             Spacer(Modifier.height(8.dp))
             Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally).background(white)){
                 Column(Modifier.fillMaxWidth().padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally){
@@ -237,7 +237,7 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
                 Text("- It must be strong;\n" +
                         "- At least one surface of the outer packaging shall have a minimum dimension of 100 x 100 mm",
                     Modifier.align(Alignment.Start),style= customTypography.bodySmall)
-                if(category == "Category A" || category == "Category B"){
+                if(category == Category.A || category == Category.B){
                     Text("- It must be UN approved Class 6.2",
                         Modifier.align(Alignment.Start),style= customTypography.bodySmall)
                 }
@@ -247,7 +247,7 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
                 alignment = Alignment.Center
             )}
         }
-        if(category == "Category A"){
+        if(category == Category.A){
         Spacer(Modifier.height(8.dp))
         Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally).background(white)){
             Column(Modifier.fillMaxWidth().padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally){
@@ -260,13 +260,13 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
         Spacer(Modifier.height(8.dp))
         Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally).background(white)){
             Column(Modifier.fillMaxWidth().padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally){
-                if(category == "Category A"){
+                if(category == Category.A){
                     Text("Put the secondary packaging into the outer UN approved Class 6.2(4GU).\n" +
                             "An itemized list of contents shall be enclosed between the secondary container and outer packaging, including the proper shipping name and technical name in parentheses of the biological agent present in the infectious substance. When the infectious substance to be transported are unknown, but suspected of meeting the criteria for inclusion in Category A, the words “suspected Category A infectious substance” must be shown in parentheses following the proper shipping name.\n" +
                             "If the packaging is not approved 4GU/CLASS 6.2..., you must verify that your packaging is the same as the prototype tested.",
                         Modifier.align(Alignment.Start),style= customTypography.bodySmall)
                 }
-                else if(category == "Category B"){
+                else if(category == Category.B){
                     Text("Put the suitable cushioning material and the secondary packaging into the outer packaging\n" +
                             "An itemized list of contents shall be enclosed between the secondary container and outer packaging.\n" +
                             "Close the Outer packaging",
@@ -281,7 +281,7 @@ fun Triplepackagingsystem(modifier:Modifier,category:String,ice:Boolean){
                 alignment = Alignment.Center
             )}
         }
-        if(category == "Category B"){
+        if(category == Category.B){
         Spacer(Modifier.height(8.dp))
         Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally).background(white)){
             Column(Modifier.fillMaxWidth().padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally){
